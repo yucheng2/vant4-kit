785a43dc241d6c07ab639b1356a2746320995fc5
yucheng2 <17835204735@163.com>
2025-06-26 10:59:46 +0800

    docs(CodeViewer): 更新DEMO_RUN_URL为生产环境地址

--- docs/.vitepress/theme/components/CodeViewer.vue
+++ docs/.vitepress/theme/components/CodeViewer.vue
@@ -28,8 +28,9 @@ import PreviewIcon from './preview-icon.vue'
 import NoPreviewIcon from './no-preview-icon.vue'
 import { useClipboard } from '@vueuse/core'
 import { showFailToast, showSuccessToast } from 'vant'
-const DEMO_RUN_URL = 'http://localhost:5173'
+// const DEMO_RUN_URL = 'http://localhost:5173'
 // const DEMO_RUN_URL = 'https://vant4-kit-moblie.netlify.app'
+const DEMO_RUN_URL = 'https://yucheng2.github.io/vant4-kit'
 const props = defineProps({
     code: {
         type: String,
